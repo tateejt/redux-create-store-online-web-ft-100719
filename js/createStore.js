168dfca0583007f4c19a4c5517412c4406987569
Emery Tate <tateejt@gmail.com>
2020-03-14 02:14:01 +0000

    Automatically backed up by Learn

--- js/createStore.js
+++ js/createStore.js
@@ -1,5 +1,10 @@
 function() {
   let state;
+  
+  function dispatch(action){
+  state = reducer(state, action);
+  render();
+};
 }
 
 function reducer(state = { count: 0 }, action) {
@@ -12,10 +17,6 @@ function reducer(state = { count: 0 }, action) {
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
 
 function render() {
   let container = document.getElementById('container');
